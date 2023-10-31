fbc4b6e0a4fd20c5a14f186b108603f531afa3d7
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-10-31 01:16:40 -0300

    update-front-tde

--- TDE-FRONT/script/script.js
+++ TDE-FRONT/script/script.js
@@ -1,21 +1,8 @@
 var radio = document.querySelector('.manual-btn')
 var cont = 1 
+const swiper = document.querySelector('.swiper').swiper;
 
-setInterval(() => {
-    proxImg()
-}, 5000)
-
-function proxImg(){
-    cont++
-
-    if (cont > 4){
-        cont = 1
-    }
-
-    document.getElementById('radio'+cont).checked = true
-}
-
-//ABRINDO AS CATEGORIAS
+//ABRINDO E FECHAnDO AS CATEGORIAS
 var nav_categorias = document.getElementById("nav-categorias")
 var categorias = document.getElementById("categorias")
 var btn_categorias = document.getElementById("tds-categorias")
@@ -32,3 +19,52 @@ categorias.addEventListener("mouseleave", function(){
     categorias.style.display = "none"
 })
 
+
+setTimeout(function () {
+
+  swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: true,
+    },
+
+    speed:500,
+
+    //Setando a Direção
+    direction: 'horizontal',
+    loop: true,
+  
+    // Setando a paginação
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //definindo as setinhas de navegação
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  }, 1000);
+  
+
+swiper = new Swiper('.swiper', {
+
+    speed:1300,
+
+    //Setando a Direção
+    direction: 'horizontal',
+    loop: true,
+  
+    // Setando a paginação
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //definindo as setinhas de navegação
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
