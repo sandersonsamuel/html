53451634ff2f06e215ae1c9dcb0a02a17a989d15
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-10-31 01:15:06 -0300

    ADD

--- TDE-FRONT/script/script.js
+++ TDE-FRONT/script/script.js
@@ -14,3 +14,21 @@ function proxImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+
+//ABRINDO AS CATEGORIAS
+var nav_categorias = document.getElementById("nav-categorias")
+var categorias = document.getElementById("categorias")
+var btn_categorias = document.getElementById("tds-categorias")
+
+btn_categorias.addEventListener("mouseenter", function(){
+    categorias.style.display = "flex"
+    
+})
+nav_categorias.addEventListener("mouseleave", function(){
+    categorias.style.display = "none"
+})
+
+categorias.addEventListener("mouseleave", function(){
+    categorias.style.display = "none"
+})
+
